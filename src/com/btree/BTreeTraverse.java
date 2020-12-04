173e1a5831c63f64afae662a0ca37139add7081b
jakondap <jagadish.kondapalli@gmail.com>
2020-12-03 21:44:47 -0600

    added few methods

--- src/com/btree/BTreeTraverse.java
+++ src/com/btree/BTreeTraverse.java
@@ -14,8 +14,11 @@ public class BTreeTraverse {
 		BTreeTraverse bTreeTraverse = new BTreeTraverse();
 		bTreeTraverse.initBTree();
 		
-		bTreeTraverse.dfsExplore(bTreeTraverse.root);
+		//bTreeTraverse.dfsExplore(bTreeTraverse.root);
 		//bTreeTraverse.bfsExplore(bTreeTraverse.root);
+		//bTreeTraverse.inOrderTraverse(bTreeTraverse.root);
+		//bTreeTraverse.levelTraverse(bTreeTraverse.root);
+		bTreeTraverse.findNode(bTreeTraverse.root, 0);
 	}
 	private void initBTree() {
 		root = new BTreeNode();
@@ -88,5 +91,139 @@ public class BTreeTraverse {
 		if(exploreQueue.size() != 0) {
 			bfsExplore(exploreQueue.remove());
 		}
+	}
+	
+	private void inOrderTraverse(BTreeNode root) {
+		BTreeNode temp = root;
+		
+		while(temp!=null && temp.getLeft()!=null) {
+			exploreStack.push(temp);
+			temp = temp.getLeft();
+		}
+		temp = exploreStack.pop();
+		String printStr = "";
+		
+		while(temp != root) {
+			if(temp.getLeft()!=null) {
+				printStr += temp.getLeft().getWeight();
+			}
+			printStr +="\t"+temp.getWeight();
+			
+			if(temp.getRight()!=null) {
+				printStr += "\t"+ temp.getRight().getWeight();
+			}
+			 System.out.print("\t"+printStr);
+			 temp = exploreStack.pop();
+		}
+		System.out.print("\t"+temp.getWeight());
+		
+		temp = root;
+		while(temp!=null && temp.getRight()!=null) {
+			exploreStack.push(temp);
+			temp = temp.getRight();
+		}
+		
+		temp = exploreStack.pop();
+		while(temp != root) {
+			if(temp.getLeft()!=null) {
+				printStr += temp.getLeft().getWeight();
+			}
+			printStr +="\t"+temp.getWeight();
+			
+			if(temp.getRight()!=null) {
+				printStr += temp.getRight().getWeight();
+			}
+			 System.out.println(printStr+"\t");
+			 temp = exploreStack.pop();
+		}
+		
+		
+	}
+	private void addElement(BTreeNode node) {
+		int leftLevel = 0;
+		int rightLevel = 0;
+		if(root == null) {
+			root = node;
+		}else if(root.getLeft() == null) {
+			root.setLeft(node);
+		}else if(root.getLeft() == null) {
+			root.setRight(node);
+		}else {
+			BTreeNode temp = root;
+			while(temp.getLeft() != null) {
+				if(temp.getLeft() == null) {
+					temp.setLeft(node);
+					return;
+				}	
+				else if(temp.getRight() == null) {
+					temp.setRight(node);
+					return;
+				}	
+				else if(temp.getLeft() != null && temp.getRight() != null) {
+					leftLevel++;
+					temp = temp.getLeft();
+				}
+			}
+			temp = root;
+			while(temp.getRight() != null) {
+				if(temp.getLeft() == null) {
+					temp.setLeft(node);
+					return;
+				}	
+				else if(temp.getRight() == null) {
+					temp.setRight(node);
+					return;
+				}	
+				else if(temp.getLeft() != null && temp.getRight() != null) {
+					rightLevel++;				
+				}
+				temp = temp.getLeft();				
+			}
+			
+		}
+	}
+	
+	
+	private  BTreeNode findNode(BTreeNode root, int nodeToFind) {
+		exploreQueue = new ArrayDeque <BTreeNode>();
+		BTreeNode temp = root;
+		
+		System.out.print(root.getWeight()+"\t");
+		
+		if(root.getLeft() != null) {
+			exploreQueue.add(root.getLeft());
+			System.out.print(root.getLeft().getWeight()+"\t");
+			
+			if(nodeToFind== temp.getLeft().getWeight())
+				return temp.getLeft();
+		}
+		if(root.getRight() != null) {
+			exploreQueue.add(temp.getRight());
+			System.out.print(temp.getRight().getWeight()+"\t");
+			
+			if(nodeToFind == temp.getRight().getWeight())
+				return temp.getRight();
+		}
+		while(!exploreQueue.isEmpty()) {
+			temp = exploreQueue.remove();
+			
+			if(temp.getLeft() != null) {
+				exploreQueue.add(temp.getLeft());
+				System.out.print(temp.getLeft().getWeight()+"\t");
+				
+				if(nodeToFind == temp.getLeft().getWeight())
+					return temp.getLeft();
+			}
+			if(temp.getRight() != null) {
+				exploreQueue.add(temp.getRight());
+				System.out.print(temp.getRight().getWeight()+"\t");
+				
+				if(nodeToFind == temp.getRight().getWeight())
+					return temp.getRight();
+			}
+			
+		}
+		return null;
+		
 	}
 }
